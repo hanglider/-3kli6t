df58d22dd1bbd4b266ee7663b4edba5bbbda54df
Ivan Trofimov <trovimovbro@gmail.com>
2024-02-27 18:22:19 +0300

    [tests] Added tests

--- projects/tests/src/radix_sort.cpp
+++ projects/tests/src/radix_sort.cpp
@@ -1,5 +1,6 @@
 #include "radix_sort.h"
 
+/*
 template <typename T>
 void RadixSort::sort_by_radix(T arr[], const T n) {
     T max = find_max(arr, n);
@@ -39,4 +40,78 @@ int RadixSort::find_max(T arr[], T n) {
         }
     }
     return max;
+}*/
+
+/*
+template<typename T>
+T RadixSort::findMax(vector<T>& arr, T l, T r) {
+    T max = arr[l];
+    for (auto i = l + 1; i < r; i++) {
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+
+template<typename T, typename S>
+void RadixSort::count_sort(vector<T>& arr, S base, T exp, T l, T r) {
+    auto n = r - l;
+    vector<T> output(n);
+    vector<T> count(base, 0);
+
+    for (int i = l; i < r; i++) {
+        count[(arr[i] / exp) % base]++;
+    }
+    for (int i = 1; i < base; i++) {
+        count[i] += count[i - 1];
+    }
+    for (int i = r - 1; i >= l; i--) {
+        output[count[(arr[i] / exp) % base] - 1] = arr[i];
+        count[(arr[i] / exp) % base]--;
+    }
+    for (int i = l; i < r; i++) {
+        arr[i] = output[i];
+    }
+}
+
+template <typename T, typename S>
+void RadixSort::sort_by_radix(vector<T>& a, S base, T l, T r) {
+    auto max = findMax(a, l, r);
+    for (T exp = l + 1; max / exp > 0; exp *= base) {
+        count_sort(a, base, exp, l, r);
+    }
+}*/
+
+int RadixSort::findMax(vector<int>& arr, int l, int r) {
+    auto max = arr[l];
+    for (auto i = l + 1; i < r; i++) {
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+
+void RadixSort::count_sort(vector<int>& arr, int base, int exp, int l, int r) {
+    auto n = r - l;
+    vector<int> output(n);
+    vector<int> count(base, 0);
+
+    for (int i = l; i < r; i++) {
+        count[(arr[i] / exp) % base]++;
+    }
+    for (int i = 1; i < base; i++) {
+        count[i] += count[i - 1];
+    }
+    for (int i = r - 1; i >= l; i--) {
+        output[count[(arr[i] / exp) % base] - 1] = arr[i];
+        count[(arr[i] / exp) % base]--;
+    }
+    for (int i = l; i < r; i++) {
+        arr[i] = output[i];
+    }
+}
+
+void RadixSort::sort_by_radix(vector<int>& a, int base, int l, int r) {
+    auto max = findMax(a, l, r);
+    for (auto exp = l + 1; max / exp > 0; exp *= base) {
+        count_sort(a, base, exp, l, r);
+    }
 }
--- projects/tests/test/test_radix.cpp
+++ projects/tests/test/test_radix.cpp
@@ -4,15 +4,13 @@
 #include "radix_sort.h"
 
 TEST(ArraysEqual, AnyElementsCount) {
-	int actual[] = {1, 8, 2, 5, 3, 11};
-    const int actual_size = sizeof(actual) / sizeof(actual[0]);
-	RadixSort::sort_by_radix(actual, actual_size);
-	int expected[] = {1, 4, 3, 5, 8, 11};
-    const int expected_size = sizeof(expected) / sizeof(expected[0]);
-	ASSERT_EQ(expected_size, actual_size)
+    vector<int> actual = {1, 8, 2, 5, 3, 11};
+	RadixSort::sort_by_radix(actual, 10, 0, actual.size());
+    vector<int> expected = {1, 4, 3, 5, 8, 11};
+	ASSERT_EQ(expected.size(), actual.size())
 		<< "Разные размеры ожидаемого и отсортированного массивов";
 	
-	for (int i = 0; i < actual_size; i++) {
+	for (int i = 0; i < actual.size(); i++) {
 		ASSERT_EQ(expected[i], actual[i]) 
 			<< "Массив ожидаемый и отсортировнный отличаются в элементе с индексом "
 			<< i;
@@ -22,4 +20,5 @@ TEST(ArraysEqual, AnyElementsCount) {
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
+    system("PAUSE");
 }
